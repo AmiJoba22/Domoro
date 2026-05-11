93e60b1b0d664ddb85e98d497fc6754619de4849
AmiJoba22 <aminajobarteh40@gmail.com>
2026-05-11 00:00:07 -0400

    footer edit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,16 +5,19 @@ export default function Footer() {
 <div>
       <div className='bg-nav-domo'>
         <a href="Home.tsx">
-            <img className= 'w-[100px] h-[100px]' src='/assets/domoro.imgs/Dom_final_logo.png' alt="domoro logo">
+            <img className= 'w-[100px] h-[100px] flex items-center justify-between ml-20' src='/assets/domoro.imgs/Dom_final_logo.png' alt="domoro logo">
                </img>
              </a> 
-        <p className= 'text-white font-domo-head'> Get a taste of The Gambia right here in NYC.<br></br> We keep the culture alive.<br></br> Order some tatsy foods today!</p>
-      </div>
+        <p className= 'text-white font-domo-head flex items-center justify-between ml-20'> Get a taste of The Gambia right here in NYC.<br></br> We keep the culture alive.<br></br> Order some tatsy foods today!</p>
+        <br>
+        </br>
+        <button className='text-white font-domo-head ml-20'> BACK TO TOP </button>
 
-      <div className='bg-domo-bg text-white font-domo-head'>  
+        <div className='bg-nav-domo text-center text-white font-domo-head'>  
         <p> Copyright © 2024 Domoro™</p>
-        <p>Email:Domoro@restaurants.org</p>
       </div>
+      </div>
+      
    
 </div>
 
